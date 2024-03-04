370ee3b0845de34c429a8770aab6885806192152
Kasia_Ngk <ngk.kasia@gmail.com~>
2024-03-04 20:49:39 +0100

    NavBar ok without - font-family

--- src/assets/Components/Home/Home.jsx
+++ src/assets/Components/Home/Home.jsx
@@ -8,16 +8,17 @@ function Home() {
 
     return (
         <div>
-            <NavBar></NavBar>
-            <div className="home"
-            style={{backgroundImage: {BGhome}}}>
-                <div className='home-txt'>
-                    <h3>SO, U WANT 2 TRAVEL 2</h3>
-                    <h1>SPACE</h1>
-                    <p>Let's face it; if u want 2 go 2 space, u might as well genuinely go 2 outer space and meet other cultures.</p>
-                </div>
-                <div className='home-btn'>
-                    <Link to='/'>EXPLORE</Link>
+            <div className='home-og'>
+                <NavBar></NavBar>
+                <div className="home">
+                    <div className='home-txt'>
+                        <h3>SO, U WANT 2 TRAVEL 2</h3>
+                        <h1>SPACE</h1>
+                        <p>Let's face it; if u want 2 go 2 space, u might as well genuinely go 2 outer space and meet other cultures.</p>
+                    </div>
+                    <div className='home-btn'>
+                        <Link to='/'>EXPLORE</Link>
+                    </div>
                 </div>
             </div>
         </div>
--- src/assets/Components/NavBar/NavBar.jsx
+++ src/assets/Components/NavBar/NavBar.jsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 import './NavBar.css'
+import Logo from '../../../../../img/shared/logo.svg'
 
 
 function NavBar() {
     return (
         <div>
             <div className="navbar">
-                <ul className="nav-links">
-                    <Link to="/">HOME</Link>
-                    <Link to="/destinationcards/0">DESTINATION</Link>
-                    <Link to="/crew">CREW</Link>
-                    <Link to="/technology/0">TECHNOLOGY</Link>
-                </ul>
+                <div className="nav-left">
+                    <img src={Logo}></img>
+                    <p>______________________________</p>
+                </div>
+                <div className="nav-right">
+                    <ul className="nav-links">
+                        <Link to="/">00 HOME</Link>
+                        <Link to="/destinationcards/0">01 DESTINATION</Link>
+                        <Link to="/crew">02 CREW</Link>
+                        <Link to="/technology/0">03 TECHNOLOGY</Link>
+                    </ul>
+                </div>
             </div>
         </div>
     )
